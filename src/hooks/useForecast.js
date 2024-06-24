9e0e18998b6f6e70215e111b1aa0eaebc4f21808
yablonya <yablonskyi.danylo@lll.kpi.ua>
2024-06-24 23:10:38 +0300

    set default location if current user location is unavailable

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -6,20 +6,32 @@ const useForecast = () => {
   const [forecastData, setForecastData] = useState(null);
 
   useEffect(() => {
+    const fetchForecast = async (latitude, longitude) => {
+      try {
+        const data = await getForecast(latitude, longitude);
+        console.log(data);
+        setForecastData(data);
+      } catch (error) {
+        console.error('Error obtaining forecast data:', error);
+      }
+    };
+
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
-        async (position) => {
+        (position) => {
           const { latitude, longitude } = position.coords;
-          const data = await getForecast(latitude, longitude);
-          console.log(data)
-          setForecastData(data);
+          fetchForecast(latitude, longitude);
         },
         (error) => {
           console.error('Error obtaining location:', error);
+          // Використання запасних координат при помилці з геолокацією
+          fetchForecast(50, 30);
         }
       );
     } else {
       console.error("Geolocation is not supported by this browser.");
+      // Використання запасних координат, якщо геолокація не підтримується
+      fetchForecast(50, 30);
     }
   }, []);
 
